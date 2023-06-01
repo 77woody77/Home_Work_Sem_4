d0a0fd285d07cc3da2d4114e42745882a2e95220
oleg Zhollobov <jolobov777@mail.ru>
2023-06-01 14:45:29 +0500

    task number twenty-seven

---
+++ Home_Work_027/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+
+Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+
+int Sum(int n)
+{ 
+    int sum = 0; 
+    while (Math.Abs(n) > 0) 
+    { 
+      
+        sum = sum + n % 10; 
+        n = n /10;
+
+    } 
+
+   return(sum);
+} 
+
+Console.WriteLine($"Cумма цифр в числе {n} равно {Sum(n)}");
